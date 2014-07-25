ef6673a891c9ba3b26abfcdc5925570a16c785d0
Duane Currier <duane.currier@stjude.org>
2014-07-25 12:20:07 -0500

    Initial Commit
    Includes untested passwordInput and mostly debugged action button with selectable icon sets.

---
+++ R/action-button.R
@@ -0,0 +1,55 @@
+#' Action button
+#' 
+#' Creates an action button whose value is initially zero, and increments by one
+#' each time it is pressed.
+#' 
+#' @param inputId Specifies the input slot that will be used to access the 
+#'   value.
+#' @param label The contents of the button--usually a text label, but you could 
+#'   also use any other HTML, like an image.
+#' @param styleclass The Bootstrap styling class of the button--options are 
+#'   primary, info, success, warning, danger, inverse, link or blank
+#' @param size The size of the button--options are large, small, mini
+#' @param block Whehter the button should fill the block
+#' @param icon Display an icon for the button
+#' @param specify icon set to use
+#'   \url{http://www.fontawesome.io/icons} or
+#'   \url{http://getbootstrap.com/2.3.2/base-css.html#icons}
+#' @param css.class Any additional CSS class one wishes to add to the action 
+#'   button
+#'   
+#' @family ShinySky elements
+#'   
+#'   
+#' @export
+actionButton <- function(inputId, label, styleclass = "", size = "", 
+    block = F, icon = NULL, icon.library = c("font awesome", "bootstrap"),  css.class = "", ...) {
+    if (styleclass %in% c("primary", "info", "success", "warning", 
+        "danger", "inverse", "link")) {
+        btn.css.class <- paste("btn", styleclass, sep = "-")
+    } else btn.css.class = ""
+    
+    if (size %in% c("large", "small", "mini")) {
+        btn.size.class <- paste("btn", size, sep = "-")
+    } else btn.size.class = ""
+    
+    if (block) {
+        btn.block = "btn-block"
+    } else btn.block = ""
+    
+    if (!is.null(icon)) {
+        if( !(icon.library[1] %in% c("font awesome", "bootstrap")) ){
+          icon.set = "font awesome"
+        }else{
+          icon.set = icon.library[1]
+        }
+        set = switch(icon.set,
+                     "font awesome" = "fa fa-",
+                     "bootstrap" = "icon-")
+        icon.code <- HTML(paste0("<i class='", set, icon, "'></i>"))
+    } else icon.code = ""
+    
+    tags$button(id = inputId, type = "button", class = paste("btn action-button", 
+        btn.css.class, btn.size.class, btn.block, css.class, collapse = " "), 
+        icon.code, label, ...)
+} 
---
+++ R/password-input.R
@@ -0,0 +1,69 @@
+#' Password Input
+#' 
+#' Creates a password input element that obscures any text entered.  The value is 
+#' not encrypted before transmission to the server.
+#' 
+#' @param inputId Specifies the input slot that will be used to access the 
+#'   value.
+#' @param label The contents of the button--usually a text label, but you could 
+#'   also use any other HTML, like an image.
+#' @param value of the input element
+#'   
+#' @family ShinyThings elements
+#'   
+#'   
+#' @export
+passwordInput <- function(inputId, label, value = "") {
+  tagList(
+    # This makes web page load the JS file in the HTML head.
+    # The call to singleton ensures it's only included once
+    # in a page.
+    shiny::singleton(
+      shiny::tags$head(
+        shiny::tags$script(src = "password-input-binding.js")
+      )
+    ),
+    shiny::tags$label(label, `for` = inputId),
+    shiny::tags$input(id = inputId, type = "password", value = value)
+  )
+}
+
+
+#' Update Password Input
+#' 
+#' Updates a password input element.
+#' 
+#' @param session identifier
+#' @param inputId Specifies the input slot that will be used to access the 
+#'   value.
+#' @param new label The contents of the button--usually a text label, but you could 
+#'   also use any other HTML, like an image.
+#' @param new value of the input element
+#'   
+#' @family ShinyThings elements
+#'   
+#'   
+#' @export
+# Send an update message to a password input on the client.
+# This update message can change the value and/or label.
+updatePasswordInput <- function(session, inputId,
+                           label = NULL, value = NULL) {
+  
+  message <- dropNulls(list(label = label, value = value))
+  session$sendInputMessage(inputId, message)
+}
+
+
+#' Remove Null Values
+#' 
+#' Drops NULL values from the list suuplied.
+#' 
+#' @param a vector or list of values that might contain 
+#' NULL values
+#' 
+#' @family ShinyThings helpers
+#' 
+# Given a vector or list, drop all the NULL items in it
+dropNulls <- function(x) {
+  x[!vapply(x, is.null, FUN.VALUE=logical(1))]
+}
