6679a227c46723661ae754089def79211ae8fe41
Duane Currier <Duane@duanes-macbook.gateway.pace.com>
2014-09-20 16:20:35 -0500

    Trying to fix issue with shinyapps.io deploy

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onLoad <- function(libname, pkgname) {
   require(shiny)
-  addResourcePath("shinythings", system.file("www", package = "shinythings"))
+  shiny::addResourcePath("shinythings", system.file("www", package = "shinythings"))
 }
 
 .onAttach <- function(libname, pkgname) {
